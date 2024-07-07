bdc99444cc5f4a76c0ea6c7f8f302418339ea3b8
Diego_Rafael_Harari <diego.harari@syngenta.com>
2024-07-07 22:02:15 +0200

    try

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://diegoharari.github.io/pixel-bytes",
+  base: "/pixel-bytes/",
   plugins: [react()],
 });
